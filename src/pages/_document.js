1dbfcdbeaec7f2201353610bd91df746f6c3bbb4
Brad Cornes <bradlc41@gmail.com>
2020-06-26 10:43:39 +0100

    Inline CSS

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,30 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import * as fs from 'fs'
+import * as path from 'path'
+
+class InlineStylesHead extends Head {
+  getCssLinks() {
+    return this.__getInlineStyles()
+  }
+
+  __getInlineStyles() {
+    const { assetPrefix, files } = this.context._documentProps
+    if (!files || files.length === 0) return null
+
+    return files
+      .filter((file) => /\.css$/.test(file))
+      .map((file) => (
+        <style
+          key={file}
+          nonce={this.props.nonce}
+          data-href={`${assetPrefix}/_next/${file}`}
+          dangerouslySetInnerHTML={{
+            __html: fs.readFileSync(path.join(process.cwd(), '.next', file), 'utf-8'),
+          }}
+        />
+      ))
+  }
+}
 
 export default class Document extends NextDocument {
   static async getInitialProps(ctx) {
@@ -9,7 +35,7 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <InlineStylesHead />
         <body>
           <Main />
           <NextScript />
