830f4e9e6913c68f097abacc1bb1eca73ac45b85
Adam Wathan <adam.wathan@gmail.com>
2020-06-29 15:04:20 -0400

    Increase page title leading

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,6 +1,6 @@
 export default function PageTitle({ children }) {
   return (
-    <h1 className="text-3xl leading-9 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-5xl md:leading-13">
+    <h1 className="text-3xl leading-9 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
       {children}
     </h1>
   )
