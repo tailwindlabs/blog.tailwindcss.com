68123cd4f1c5aa4493967057205777f5d2c62a3a
Adam Wathan <adam.wathan@gmail.com>
2020-06-29 15:04:07 -0400

    Add seven hundred favicon files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,20 @@
 import '@/css/tailwind.css'
+import Head from 'next/head'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
 
 export default function App({ Component, pageProps }) {
   return (
     <div className="antialiased">
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <SectionContainer>
         <Header />
       </SectionContainer>
