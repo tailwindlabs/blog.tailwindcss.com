9a06d9a198aa19bb5391f661084addd1d3b9746b
Brad Cornes <bradlc41@gmail.com>
2020-06-29 14:48:13 +0100

    Add header link hover style

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,7 +44,7 @@ export default function Header() {
         </Link>
       </div>
       <div className="text-base leading-5">
-        <a href="https://tailwindcss.com" className="font-medium text-gray-500">
+        <a href="https://tailwindcss.com" className="font-medium text-gray-500 hover:text-gray-700">
           Documentation &rarr;
         </a>
       </div>
