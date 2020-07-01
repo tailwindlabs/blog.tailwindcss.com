4bf428cf9d95d1d981cfc0ba354b99ce146eafe7
Brad Cornes <bradlc41@gmail.com>
2020-07-01 16:09:18 +0100

    Add mdx purgecss extractor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const mdx = require('@mdx-js/mdx')
 
 module.exports = {
-  purge: { content: ['./src/**/*.{js,mdx}', './next.config.js'], mode: 'all' },
+  purge: {
+    mode: 'all',
+    content: ['./src/**/*.{js,mdx}', './next.config.js'],
+    options: {
+      extractors: [
+        {
+          extensions: ['mdx'],
+          extractor: (content) => {
+            content = mdx.sync(content)
+
+            // Capture as liberally as possible, including things like `h-(screen-1.5)`
+            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+            // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+            const innerMatches =
+              content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || []
+
+            return broadMatches.concat(innerMatches)
+          },
+        },
+      ],
+    },
+  },
   theme: {
     extend: {
       lineHeight: {
