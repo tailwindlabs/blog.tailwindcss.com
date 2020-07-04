70606f5e9137f94887d0d44d88925d4b3f3f799c
Shodipo Ayomide <shodipovi@gmail.com>
2020-07-04 23:31:00 +0100

    Chore: Link to Tailwind Docs and not home page
    Current state: `Documentation →` leads to Tailwind's HomePage `https://tailwindcss.com`
    
    Changes: switched the link in `Documentation →` so we can link directly to the docs route and not the HomePage

--- src/components/Header.js
+++ src/components/Header.js
@@ -47,7 +47,7 @@ export default function Header() {
         </Link>
       </div>
       <div className="text-base leading-5">
-        <a href="https://tailwindcss.com" className="font-medium text-gray-500 hover:text-gray-700">
+        <a href="https://tailwindcss.com/docs/installation" className="font-medium text-gray-500 hover:text-gray-700">
           Documentation &rarr;
         </a>
       </div>
