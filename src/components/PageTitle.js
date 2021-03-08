4e4bfaab705db629dcaa215ce20620823fddb07b
Adam Wathan <adam.wathan@gmail.com>
2021-03-08 14:27:28 -0500

    Add post welcoming James

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,7 +1,7 @@
 export default function PageTitle({ children }) {
   return (
     <h1 className="text-3xl leading-9 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-      {children}
+      {children.replace(/ ([^ ]+)$/, '\u00A0$1')}
     </h1>
   )
 }
