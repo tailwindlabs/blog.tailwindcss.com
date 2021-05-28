94ccb4f1db3515391d423202470667d7fb18e736
Brad Cornes <bradlc41@gmail.com>
2021-05-28 17:09:14 +0100

    upgrade tailwindcss and enable jit mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,11 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const mdx = require('@mdx-js/mdx')
 
 module.exports = {
+  mode: 'jit',
   purge: {
-    mode: 'all',
     content: ['./src/**/*.{js,mdx}', './next.config.js'],
-    options: {
-      extractors: [
-        {
-          extensions: ['mdx'],
-          extractor: (content) => {
-            content = mdx.sync(content)
-
-            // Capture as liberally as possible, including things like `h-(screen-1.5)`
-            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-
-            // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-            const innerMatches =
-              content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || []
-
-            return broadMatches.concat(innerMatches)
-          },
-        },
-      ],
+    transform: {
+      mdx: mdx.sync,
     },
   },
   theme: {
